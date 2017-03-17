d70566c40fd5fb0d85f80cdd0d4b0dc1917016f1
jmoneywesterchester <jgwesterfield@gmail.com>
2017-03-17 01:07:57 -0500

    Added a new file (ProcessSQLOutput) to be worked on. It has the list of TODO's inside.

---
+++ src/ProcessSQLOutput.java
@@ -0,0 +1,8 @@
+/**
+ * Created by JonathanW on 3/17/17.
+ */
+public class ProcessSQLOutput
+{
+    //TODO: Add function that calculates average GPA
+    //TODO: Add function that calculates percentage of each grade letter given overall
+}
