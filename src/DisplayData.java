3dd3d3a625c1dfbc8fbde177fd4af6b63e5788c9
jmoneywesterchester <jgwesterfield@gmail.com>
2017-06-24 16:48:21 -0500

    set the font for all of the functions in this class

--- src/DisplayData.java
+++ src/DisplayData.java
@@ -10,8 +10,10 @@ import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.event.*;
+import javafx.scene.text.*;
 
 import javax.xml.crypto.Data;
+import java.sql.SQLException;
 import java.text.DecimalFormat;
 
 
@@ -30,7 +32,28 @@ public class DisplayData extends Application
 
     @Override public void start(Stage primaryStage)
     {
+        try
+        {
+            DatabaseAPI db = new DatabaseAPI("CSCE", 121, "MOORE");
 
+            DisplayData display = new DisplayData(db);
+
+
+            Scene scene = new Scene(display.getPercentagesDisplay(), 200, 220);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }
+        catch (SQLException | ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public DisplayData()
+    {
+        // default constructor
     }
 
     public DisplayData(DatabaseAPI database)
@@ -72,11 +95,22 @@ public class DisplayData extends Application
         double avgGPA = db.getAverageGPA();
 
         Label labelA = new Label("Numbers of A's: " + numA);
+        labelA.setFont(new Font("Futura", 15));
+
         Label labelB = new Label("Numbers of B's: " + numB);
+        labelB.setFont(new Font("Futura", 15));
+
         Label labelC = new Label("Numbers of C's: " + numC);
+        labelC.setFont(new Font("Futura", 15));
+
         Label labelD = new Label("Numbers of D's: " + numD);
+        labelD.setFont(new Font("Futura", 15));
+
         Label labelF = new Label("Numbers of F's: " + numF);
+        labelF.setFont(new Font("Futura", 15));
+
         Label labelQ = new Label("Numbers of Q's: " + numQ);
+        labelQ.setFont(new Font("Futura", 15));
 
         data.getChildren().addAll(labelA, labelB, labelC, labelD, labelF, labelQ);
 
@@ -93,32 +127,49 @@ public class DisplayData extends Application
         double percentPassing = db.getPercentageA() + db.getPercentageB() + db.getPercentageC();
 
         Label perA = new Label("Percent A:  " + df.format(db.getPercentageA()) + "%");
+        perA.setFont(new Font("Futura", 15));
+
         Label perB = new Label("Percent B:  " + df.format(db.getPercentageB()) + "%");
+        perB.setFont(new Font("Futura", 15));
+
         Label perC = new Label("Percent C:  " + df.format(db.getPercentageC()) + "%");
+        perC.setFont(new Font("Futura", 15));
+
         Label perD = new Label("Percent D:  " + df.format(db.getPercentageD()) + "%");
+        perD.setFont(new Font("Futura", 15));
+
         Label perF = new Label("Percent F:  " + df.format(db.getPercentageF()) + "%");
+        perF.setFont(new Font("Futura", 15));
+
         Label perQ = new Label("Percent Q Drops:  " + df.format(db.getPercentageQ()) + "%");
+        perQ.setFont(new Font("Futura", 15));
+
         Label perPass = new Label("Passing Rate:  " + df.format(percentPassing) + "%");
+        perPass.setFont(new Font("Futura", 15));
 
         percentages.getChildren().addAll(perA, perB, perC, perD, perF, perQ, perPass);
 
         this.percentagesDisplay = percentages;
     }
 
-    // TODO: SET THE LABELS TO A PARTICULAR FONT, CHECK OUT GARAMOND, FRUTIGER, TAHOMA, VERDANA
     // Displays what course we are analyzing
-    private void setCourseInfo()
+    //needs to be 500x55
+    private HBox setCourseInfo()
     {
         HBox info = new HBox(10);
         info.setPadding(new Insets(15, 15, 15, 15));
 
         Label course = new Label("Course: " + db.getCourseSubject() + " " +
                 db.getCourseNum());
+        course.setFont(new Font("Futura", 18));
 
         Label professor = new Label("Professor: " + db.getProfessor());
+        professor.setFont(new Font("Futura", 18));
 
         info.getChildren().addAll(course, professor);
 
         this.courseInfo = info;
+
+        return info;
     }
 }
