7c5c20e1f3e2639778bae6311c4e98c52c5b11a0
jmoneywesterchester <jgwesterfield@gmail.com>
2017-08-16 01:07:22 -0500

    Deleted unused import statements

--- src/main.java
+++ src/main.java
@@ -2,32 +2,12 @@
  * Created by JonathanWesterfield on 12/24/16.
  */
 
-import javafx.application.Application;
-import javafx.scene.control.*;
-import javafx.animation.*;
-import javafx.geometry.Insets;
-import javafx.stage.*;
-import javafx.collections.*;
-import javafx.scene.*;
-import javafx.scene.layout.*;
-import javafx.scene.image.*;
-import javafx.scene.control.*;
-import javafx.event.*;
-import javafx.scene.text.*;
 import javafx.application.*;
-import javafx.beans.value.*;
-import javafx.event.*;
-import javafx.geometry.*;
-import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.concurrent.*;
 import javax.xml.crypto.Data;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
 import java.sql.SQLException;
-import java.util.ArrayList;
-import javax.xml.crypto.Data;
 import java.io.*;
 import java.sql.SQLException;
 import java.util.*;
